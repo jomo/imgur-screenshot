ac84088d77b4af0fb8369c6511a426e140b5e9f7
jomo <git@jomo.tv>
2013-11-10 21:14:09 +0100

    Let the fun begin

---
+++ README.md
@@ -0,0 +1,18 @@
+# Imgur-Screenshot
+
+A bash script for Linux that lets you take a screenshot, select an area, (edit it), upload it to [imgur](https://imgur.com), copies the link to clipboard (and opens the link).
+
+Move the script to somewhere in your `PATH` as `imgur-screenshot`. Create a keyboard shortcut for <kbd>Print</kbd> (or any key you like) and set it to `imgur-screenshot`.
+
+**Enjoy!**
+
+Dependencies
+----
+
+Most of these are installed on most *nix systems
+
+* scrot
+* curl
+* grep
+* xclip
+* libnotify-bin
---
+++ imgur-screenshot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Imgur API key
+key="486690f872c678126a2c09a9e196ce1b"
+#Imgur favicon, for notifications
+ico="$HOME/Pictures/imgur.png"
+#Filename prefix
+pre="imgur-"
+#Image location
+save="$HOME/Pictures/"
+#Editor (before upload)
+edit="gimp"
+#Open with - May be empty
+open="firefox"
+
+
+cd "$save"
+#filename with date
+img="$pre`date +"%d.%m.%Y-%H:%M:%S"`.png"
+echo "Please select area"
+scrot -s "$img" #takes a screenshot with selection
+if [ ! -z "$edit" ]
+  then
+  echo "Opening editor $edit"
+  $edit "$img"
+fi
+echo "Uploading $img"
+response=`curl -s -F "image=@$img" -F "key=$key" https://imgur.com/api/upload.xml`
+echo "Server reponse received"
+#echo "$response" #debug
+if [[ "$response" == *"stat=\"ok\""*  ]]
+  then
+  url=`echo "$response" | egrep -o "<original_image>(.)*</original_image>" | egrep -o "http://i.imgur.com/[^<]*"`
+  echo "$url"
+  echo "$url" | xclip -selection c
+  if [ ! -z "$open" ]
+    then
+    echo "Opening URL with $open"
+    $open "$url"
+  fi
+  notify-send -a ImgurScreenshot -i "$ico" -t 500 "Imgur: Upload done! $url copied to clipboard!"
+else
+  notify-send -a ImgurScreenshot -i "$ico" -t 500 "Imgur: Upload failed :("
+fi

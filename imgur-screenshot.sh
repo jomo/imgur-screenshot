f9c7f6274fd575ab84bedbae34abc3d225e07393
jomo <github@jomo.tv>
2014-12-18 19:38:58 +0100

    fix syntax error mentioned in #23

--- imgur-screenshot.sh
+++ imgur-screenshot.sh
@@ -215,7 +215,7 @@ function save_access_token() {
   access_token="$(egrep -o 'access_token":".*"' <<<"$1" | cut -d '"' -f 3)"
   refresh_token="$(egrep -o 'refresh_token":".*"' <<<"$1" | cut -d '"' -f 3)"
   expires_in="$(egrep -o 'expires_in":".*"' <<<"$1" | cut -d '"' -f 3)"
-  token_expire_time="$(( "$(date +%s)" + expires_in ))"
+  token_expire_time="$(( $(date +%s) + expires_in ))"
 
   # create dir if not exist
   mkdir -p "$(dirname "$2")" 2>/dev/null
